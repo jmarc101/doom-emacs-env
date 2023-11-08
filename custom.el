d426188a4fd6a55bb86f4ad082daafd6cb381534
Jean-Marc Prud'homme <jm.prudhomme@optable.co>
2023-11-08 18:57:29 -0500

    Added protobuf-mode for lsp

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(calfw-ical calfw org-ql org-super-agenda yasnippet org-pretty-tags eslintd-fix)))
- (custom-set-faces
+   '(protobuf-mode calfw-ical calfw org-ql org-super-agenda yasnippet org-pretty-tags eslintd-fix)))
+ 
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
